b316d77751c9e44221bc4769388cc841b07e0c34
Vincent Kempers <vincent@vriendvandeshow.nl>
2022-08-26 19:50:10 +0200

    add dist build

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,47 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          // treat all tags with a dash as custom elements
-          isCustomElement: (tag) => tag.includes('-')
-        }
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      plugins: [
+        vue({
+          reactivityTransform: true,
+          template: {
+            compilerOptions: {
+              // treat all tags with a dash as custom elements
+              isCustomElement: (tag) => tag.includes("-"),
+            },
+          },
+        }),
+      ],
+      base: "/",
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+    };
+  } else {
+    // command === 'build'
+    return {
+      plugins: [
+        vue({
+          reactivityTransform: true,
+          template: {
+            compilerOptions: {
+              // treat all tags with a dash as custom elements
+              isCustomElement: (tag) => tag.includes("-"),
+            },
+          },
+        }),
+      ],
+      base: "/visualizer/",
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+    };
+  }
 });
